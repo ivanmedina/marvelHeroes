07ddeff5112d03b548e1f0d45a6fcd7b8962f916
Ivan Medina <ivansk8.imm@gmail.com>
2020-05-29 06:23:45 -0600

    Delete initdb.sh
    se inicializa la base de datos desde peewee

--- db/scripts/initdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "db" <<-EOSQL
-    CREATE TABLE marvelHeroes1 (id int PRIMARY KEY, nombre char(50),tipos char(150),imagen char(350));
-EOSQL
